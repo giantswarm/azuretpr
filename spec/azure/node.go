55910bfd3f21af39db408b1a0c706e193b2c2bd8
cocotton <julien.parent.trudeau@gmail.com>
2017-12-11 19:58:10 -0500

    Create a node.Disk struct containing infos about the OS and Data disks

--- spec/azure/node.go
+++ spec/azure/node.go
@@ -5,12 +5,14 @@ import "github.com/giantswarm/azuretpr/spec/azure/node"
 type Node struct {
 	// VMSize is the master vm size (e.g. Standard_A1)
 	VMSize string `json:"vmSize" yaml:"vmSize"`
-	// DataDiskSizeGB is the vm data disk size in GB
-	DataDiskSizeGB int `json:"dataDiskSizeGB" yaml:"dataDiskSizeGB"`
 	// AdminUsername is the vm administrator username
 	AdminUsername string `json:"adminUsername" yaml:"adminUsername"`
 	//  AdminSSHKeyData is the vm administrator ssh public key
 	AdminSSHKeyData string `json:"adminSSHKeyData" yaml:"adminSSHKeyData"`
+	// OSDisk is the vm OS disk object
+	OSDisk node.Disk `json:"osDisk" yaml:"osDisk"`
+	// DataDisks is an array of the vm data disks
+	DataDisks []node.Disk `json:"dataDisks" yaml:"dataDisks"`
 	// OSImage is the vm OS image object
 	OSImage node.OSImage `json:"osImage" yaml:"osImage"`
 }
